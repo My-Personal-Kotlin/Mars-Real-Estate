383de8b9452185c21cefc1f5ebc09655714bfce8
Awais Mansha <awaismansha1998@gmail.com>
2021-09-14 19:07:26 +0500

    Working on lesson 8 in Kotlin

---
+++ app/src/main/java/com/marsrealestate/detail/DetailFragment.kt
@@ -0,0 +1,32 @@
+package com.marsrealestate.detail
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.marsrealestate.R
+import com.marsrealestate.databinding.FragmentDetailBinding
+import com.marsrealestate.databinding.FragmentDetailBinding.inflate
+
+class DetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_detail, container, false)
+
+        val application = requireNotNull(activity).application
+
+        binding.lifecycleOwner = this
+
+//        val marsProperty = DetailFragmentArgs.fromBundle(requireArguments()).selectedProperty
+//        val viewModelFactory = DetailViewModelFactory(marsProperty, application)
+//        binding.viewModel = ViewModelProvider(
+//            this, viewModelFactory).get(DetailViewModel::class.java)
+        return binding.root
+    }
+}
---
+++ app/src/main/java/com/marsrealestate/overview/OverviewModel.kt
@@ -0,0 +1,96 @@
+package com.marsrealestate.overview
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.marsrealestate.network.MarsApi
+import com.marsrealestate.network.MarsApiFilter
+import com.marsrealestate.network.MarsProperty
+import kotlinx.coroutines.launch
+
+
+enum class MarsApiStatus { LOADING, ERROR, DONE }
+/**
+ * The [ViewModel] that is attached to the [OverviewFragment].
+ */
+class OverviewViewModel : ViewModel() {
+
+    // The internal MutableLiveData that stores the status of the most recent request
+    private val _response = MutableLiveData<String>()
+
+    // The external immutable LiveData for the request status
+    val response: LiveData<String>
+        get() = _response
+
+//    // Internally, we use a MutableLiveData, because we will be updating the List of MarsProperty
+//    // with new values
+//    private val _properties = MutableLiveData<List<MarsProperty>>()
+//
+//    // The external LiveData interface to the property is immutable, so only this class can modify
+//    val properties: LiveData<List<MarsProperty>>
+//        get() = _properties
+//
+//    // Internally, we use a MutableLiveData to handle navigation to the selected property
+//    private val _navigateToSelectedProperty = MutableLiveData<MarsProperty>()
+//
+//    // The external immutable LiveData for the navigation property
+//    val navigateToSelectedProperty: LiveData<MarsProperty>
+//        get() = _navigateToSelectedProperty
+//
+
+
+    /**
+     * Call getMarsRealEstateProperties() on init so we can display status immediately.
+     */
+    init {
+        getMarsRealEstateProperties()
+    }
+
+    /**
+     * Gets filtered Mars real estate property information from the Mars API Retrofit service and
+     * updates the [MarsProperty] [List] and [MarsApiStatus] [LiveData]. The Retrofit service
+     * returns a coroutine Deferred, which we await to get the result of the transaction.
+     * @param filter the [MarsApiFilter] that is sent as part of the web server request
+     */
+    private fun getMarsRealEstateProperties() {
+        _response.value = "Set the Mars Api Response here !"
+//        viewModelScope.launch {
+//            _response.value = MarsApiStatus.LOADING
+//            try {
+//              //  _properties.value = MarsApi.retrofitService.getProperties(filter.value)
+//                _response.value = MarsApiStatus.DONE
+//            } catch (e: Exception) {
+//                _response.value = MarsApiStatus.ERROR
+//             //   _properties.value = ArrayList()
+//            }
+//        }
+    }
+//
+//    /**
+//     */
+//
+//    /**
+//     * When the property is clicked, set the [_navigateToSelectedProperty] [MutableLiveData]
+//     * @param marsProperty The [MarsProperty] that was clicked on.
+//     */
+//    fun displayPropertyDetails(marsProperty: MarsProperty) {
+//        _navigateToSelectedProperty.value = marsProperty
+//    }
+//
+//    /**
+//     * After the navigation has taken place, make sure navigateToSelectedProperty is set to null
+//     */
+//    fun displayPropertyDetailsComplete() {
+//        _navigateToSelectedProperty.value = null
+//    }
+
+    /**
+     * Updates the data set filter for the web services by querying the data with the new filter
+     * by calling [getMarsRealEstateProperties]
+     * @param filter the [MarsApiFilter] that is sent as part of the web server request
+//     */
+//    fun updateFilter(filter: MarsApiFilter) {
+//        getMarsRealEstateProperties(filter)
+//    }
+}
